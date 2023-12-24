fb613ef5e6dcc486b99ec07dde241e7b53aae09f
Trúc Phạm <phamtruc.main@gmail.com>
2023-12-24 19:54:12 +0700

    Added Meal Item Description

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Fragment } from "react";
 import Header from "./components/Header";
-
+import Meals from "./pages/Meals";
 function App() {
   return (
     <Fragment>
       <Header></Header>
+      <Meals></Meals>
     </Fragment>
   );
 }
